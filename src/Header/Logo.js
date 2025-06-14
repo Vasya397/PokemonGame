d68462d173ebb5c9dc2df2a4ce9adff2e29cc1b5
Vasilii <vabednovcska@gmail.com>
2025-06-14 17:42:58 +0500

    Добавил + - баланс, грид сетку 1X1 и 2X2

--- src/Header/Logo.js
+++ src/Header/Logo.js
@@ -2,8 +2,36 @@ import styled from './Header.module.css'
 import myImage1 from "../img/image1.png"
 import myImage2 from "../img/image2.png"
 import myImage3 from "../img/image3.png"
+import { useDispatch, useSelector } from 'react-redux'
+
+const defaulState = {
+    cash: 100500,
+  }
+
+export const reducerLogo = (state = defaulState, action) => {
+  switch(action.type) {
+    
+    case "PLUS_BALANCE":
+      return {...state, cash: state.cash + action.payload}
+    case "MINUS_BALANCE":
+      return {...state, cash: state.cash - action.payload}
+    default: 
+      return state
+  }
+}
 
 export function Logo() {
+    const plusCash = () => {
+    dispatch({type: "PLUS_BALANCE", payload: 1000})
+  }
+
+  const minusCash = () => {
+    dispatch({type: "MINUS_BALANCE", payload: 1000})
+  }
+
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash)
+
   return ( 
     <div className={styled.container}> 
       <div> 
@@ -16,14 +44,18 @@ export function Logo() {
       <img src={myImage3} alt='3' />
       </div>
       <div className={styled.money}> 
-        100500
+        {cash}
       </div>
-      <div className={styled.plusMinus}> 
-        <div className={styled.plusBalance}> 
-          +
+      <div> 
+        <div className={styled.plusBalance}>
+          <button onClick={() => plusCash()}> 
+            +
+          </button>
         </div>
         <div className={styled.plusBalance}> 
-          -
+          <button onClick={() => minusCash()}> 
+            -
+          </button>
         </div> 
     </div>
     </div>
--- src/Inventory/Inventory.js
+++ src/Inventory/Inventory.js
@@ -6,13 +6,18 @@ export function Inventory() {
     <div className={styled.container}>
       <div className={styled.slotText}> Inventory </div>
         <div className={styled.slots}> 
-          <div className={styled.slot}></div>
-          <div className={styled.slot}></div>
-          <div className={styled.slot}></div>
-          <div className={styled.slot}></div>
-          <div className={styled.slot}></div>
-          <div className={styled.slot}></div>
-          <div className={styled.slot}></div>
+          <div className={styled.slot1X1}></div>
+          <div className={styled.slot1X1}></div>
+          <div className={styled.slot1X1}></div>
+          <div className={styled.slot1X1}></div>
+          <div className={styled.slot1X1}></div>
+          <div className={styled.slot1X1}> </div>
+          <div className={styled.slot1X1}> </div>
+          <div className={styled.slot1X1}> </div>
+          <div className={styled.slot2X2}> </div>
+          <div className={styled.slot1X1}> </div>
+          <div className={styled.slot1X1}> </div>
+          <div className={styled.slot1X1}> </div>
         </div>
       <ButtonMoney /> 
     </div>
